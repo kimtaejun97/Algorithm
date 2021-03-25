775574b0ff465c7e69aafebdfde1fb5a60f19433
kimtaejun <kim_taejun@naver.com>
2021-03-25 23:58:32 +0900

    pseudocode

---
+++ src/programmers/MenuRenewal.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class MenuRenewal {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+
+        //알파벳 bool 배열
+        //각 알파벳이(음식)이 몇번씩 등장했는지 저장하는 배열.
+        //count가 2보다 작으면 고려하지 않음.
+        //코스의 길이보다 같거나 큰 오더의 수가 2보다 작으면 탐색하지 않음.
+
+
+        return answer;
+    }
+}
---
+++ src/programmers/괄호변환.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class 괄호변환 {
+    public String solution(String p) {
+        String answer = "";
+
+        // u와 v의 분리 -> ->단말 균형잡인 문자열 ->U, 나머지 ->v
+        //u가 올바른 문자열이면 그대로 두고 v에 대해여 다시 u v 분리.(재귀)
+        //u가 올바르지 않으면 u에 대하여 맨앞, 맨 뒤의 문자를 제거.
+        // '('를 붙이고 나머지 괄호 방향을 뒤집어서 붙이고 ')' 븉임. 반환
+        // 반환값을 모두 이어 붙임.
+
+        return answer;
+    }
+}
