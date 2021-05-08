572c339d08315e4ef1031cd6209d51a64eb7194c
kimtaejun <kim_taejun@naver.com>
2021-05-08 12:50:35 +0900

    키패드 누르기

--- src/programmers/다단계칫솔판매.java
+++ src/programmers/다단계칫솔판매.java
@@ -18,7 +18,7 @@ public class 다단계칫솔판매 {
         for(int i=0; i< seller.length; i++){
             int currentProfit = amount[i] * PRICE;
             String target = seller[i];
-            while(target !="-"){
+            while(!target.equals("-")){
                 if(currentProfit * 0.1 >=1)
                     profit.put(target, profit.get(target)+(int)Math.ceil(currentProfit *0.9));
                 else {
@@ -37,19 +37,4 @@ public class 다단계칫솔판매 {
 
         return answer;
     }
-
-    public static void main(String[] args) {
-        다단계칫솔판매 o = new 다단계칫솔판매();
-        String[] e = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
-        String[] r = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
-        String[] s = {"young", "john", "tod", "emily", "mary"};
-        int[] a = {12,4,2,5,10};
-        int[] ans =o.solution(e, r, s, a);
-
-        for(int v :ans)
-            System.out.println(v);
-    }
-    // 2 30 50 40
-
-
 }
--- src/programmers/키패드누르기.java
+++ src/programmers/키패드누르기.java
@@ -1,8 +1,72 @@
 package programmers;
 
+import java.util.HashMap;
+import static java.lang.Math.abs;
+
 public class 키패드누르기 {
     public String solution(int[] numbers, String hand) {
         String answer = "";
+        HashMap<Integer, int[]> leftNumber = new HashMap<>(){
+            {
+                put(1, new int[]{0, 0});
+                put(4, new int[]{1, 0});
+                put(7, new int[]{2, 0});
+            }
+        };
+        HashMap<Integer, int[]> centerNumber = new HashMap<>(){
+            {
+                put(2, new int[]{0, 1});
+                put(5, new int[]{1, 1});
+                put(8, new int[]{2, 1});
+                put(0, new int[]{3, 1});
+            }
+        };
+        HashMap<Integer, int[]> rightNumber = new HashMap<>(){
+            {
+                put(3, new int[]{0, 2});
+                put(6, new int[]{1, 2});
+                put(9, new int[]{2, 2});
+            }
+        };
+
+        int[] leftLocation= {3,0};
+        int[] rightLocation ={3,2};
+        int[] touchLocation;
+        int leftDist=0;
+        int rightDists=0;
+
+
+
+        for(int touch : numbers){
+            if((touchLocation = leftNumber.get(touch))!=null){
+                answer+="L";
+                leftLocation[0] = touchLocation[0];
+                leftLocation[1] = touchLocation[1];
+            }
+            else if ((touchLocation = rightNumber.get(touch))!=null){
+                answer+="R";
+                rightLocation[0] = touchLocation[0];
+                rightLocation[1] = touchLocation[1];
+            }
+            else{
+                touchLocation = centerNumber.get(touch);
+                leftDist = abs(leftLocation[0] -touchLocation[0]) + abs(leftLocation[1] - touchLocation[1]);
+                rightDists = abs(rightLocation[0] -touchLocation[0]) + abs(rightLocation[1] - touchLocation[1]);
+
+                if(leftDist < rightDists || (leftDist==rightDists && hand.equals("left"))){
+                    answer+="L";
+                    leftLocation[0] = touchLocation[0];
+                    leftLocation[1] = touchLocation[1];
+                }
+                else if(rightDists < leftDist || (leftDist == rightDists && hand.equals("right"))){
+                    answer+="R";
+                    rightLocation[0] = touchLocation[0];
+                    rightLocation[1] = touchLocation[1];
+                }
+
+            }
+        }
+
         return answer;
     }
 
