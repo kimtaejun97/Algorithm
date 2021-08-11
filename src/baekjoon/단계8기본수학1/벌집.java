b1eae573df930c0528472ae07f938314fa9dcba2
kimtaejun <kim_taejun@naver.com>
2021-08-11 20:12:19 +0900

    기본수학:벌집

---
+++ src/baekjoon/단계8기본수학1/벌집.java
@@ -0,0 +1,41 @@
+package baekjoon.단계8기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2-7, 8 - 19, 20-37...
+// range : 6( n + (n-1) + (n-2)...) +2 - > 3n(n+1) + 2 ~ 3n(n+1) + 6(n+1) +1
+// range : 3n(n+1) +2 ~ 3n(n+3) +7
+// n+1 개의 방을 들러야 함.
+public class 벌집 {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input  = Integer.parseInt(br.readLine());
+        int n = 0;
+
+        if(input == 1){
+            System.out.println(1);
+        }
+        else{
+            while(true){
+                if((input >= 3*n*(n+1) + 2) && input <= 3*n*(n+3) + 7 ){
+                    break;
+                }
+                n++;
+            }
+            
+            System.out.println(n+2);
+        }
+
+        // 또는 초깃값부터 6n 씩 더해가며 이보다 작은지 검사.
+
+
+
+
+    }
+}
