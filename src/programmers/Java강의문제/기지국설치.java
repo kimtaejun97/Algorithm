937df97551dd440edd2a2ffa122413b7c7bd733f
kimtaejun <kim_taejun@naver.com>
2021-10-12 23:11:27 +0900

    그리디 알고리즘: 기지국설치

---
+++ src/programmers/Java강의문제/기지국설치.java
@@ -0,0 +1,25 @@
+package programmers.Java강의문제;
+
+public class 기지국설치 {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int stationPointer = 0;
+
+        int position = 1;
+        while(position <= n){
+            // 전 탐색지점 ~ 이번 탐색 지점 사이에 이미 설치된 기지국이 있다면 해당 기지국의 영향권 밖으로 이동.
+            if(stationPointer < stations.length
+                    && stations[stationPointer] - w <= position){
+                position = stations[stationPointer] + w + 1;
+                stationPointer ++;
+            }
+            else{
+                answer ++;
+
+                // w 떨어진 곳에 기지국을 설치하고 포인터를 영향권 밖으로 옮김.
+                position += 2*w + 1;
+            }
+        }
+        return answer;
+    }
+}
