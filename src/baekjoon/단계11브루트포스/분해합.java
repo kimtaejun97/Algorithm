7f7e210bf7e5406d53de3060808d59a7b20c7873
kimtaejun <kim_taejun@naver.com>
2021-10-15 20:30:59 +0900

    브루트포스 : 분해합

--- src/baekjoon/단계11브루트포스/분해합.java
+++ src/baekjoon/단계11브루트포스/분해합.java
@@ -4,44 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static java.lang.System.in;
+
 public class 분해합 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String n = br.readLine();
+        int num = Integer.parseInt(n);
 
-        int sum=0;
-        int constructor = 0;
-        int answer = 0;
+        int cons = 0;
+        for(int i=num - n.length() * 9; i<num; i++){
+            int candidate = i;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String stringNumber = br.readLine();
-
-        int maxGap = stringNumber.length() * 9;
-        int number = Integer.valueOf(stringNumber);
-
-        int start = number - maxGap;
-
-        for(int estimate = start; estimate<number-2; estimate++){
-            String stringEstimate = String.valueOf(estimate);
-            sum = estimate;
-            for(int i =0; i<stringEstimate.length(); i++){
-                String substring = String.valueOf(stringEstimate.charAt(i));
-                sum += Integer.valueOf(substring);
+            int sum = candidate;
+            while(candidate > 0){
+                sum += candidate % 10;
+                candidate /= 10;
             }
 
-            if(sum == number){
-                answer = estimate;
+            if(sum == num){
+                cons = i;
                 break;
             }
-
         }
-
-        System.out.println(answer);
-
-
-
-
-
-
-
+        System.out.println(cons);
     }
 }
--- src/baekjoon/단계11브루트포스/체스판다시칠하기.java
+++ src/baekjoon/단계11브루트포스/체스판다시칠하기.java
@@ -7,46 +7,6 @@ import java.util.StringTokenizer;
 
 public class 체스판다시칠하기 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
 
-        int height = Integer.parseInt(stringTokenizer.nextToken());
-        int width = Integer.parseInt(stringTokenizer.nextToken());
-
-        int expected = 0;
-        char[] wb = {'W', 'B'};
-        int result = height * width;
-
-        String [] block = new String[height];
-
-        for(int i = 0; i<height; i++){
-            block[i] = br.readLine();
-        }
-
-        for(int h = 0; h+7<height; h++){
-            for(int w = 0; w+7<width; w++){
-                if(block[h].charAt(w) =='W'){
-                    expected = 0;
-                }
-                else{
-                    expected = 1;
-                }
-                int wrong = 0;
-
-                for(int mh = h; mh<=h+7; mh++){
-                    for(int mw = w; mw<=w+7; mw++){
-                        if(block[mh].charAt(mw) != wb[expected]){
-                            wrong ++;
-                        }
-                        if(mw != w+7){
-                            expected = (expected+ 1) % 2;
-                        }
-                    }
-                }
-                if(wrong < result)
-                    result = wrong;
-            }
-        }
-        System.out.println(result);
     }
 }
