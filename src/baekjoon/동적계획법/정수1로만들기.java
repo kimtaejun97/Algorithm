0161b0429ab4008a0afbb8414c9f92df0ed8a28c
kimtaejun <kim_taejun@naver.com>
2021-10-21 18:07:55 +0900

    동적계획법: 1로만들기

--- src/baekjoon/동적계획법/정수1로만들기.java
+++ src/baekjoon/동적계획법/정수1로만들기.java
@@ -7,14 +7,27 @@ import java.io.InputStreamReader;
 import static java.lang.System.in;
 
 public class 정수1로만들기 {
+    static int MAX = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in));
         int n = Integer.parseInt(br.readLine());
-        int count = 0;
+        int count[] = new int[n+1];
+        if(n <=3){
+            if(n == 1) System.out.println(0);
+            else System.out.println(1);
+            System.exit(0);
+        }
 
+        for(int i=0; i<n+1; i++){
+            count[i] = MAX;
+        }
+        count[2] = count[3] = 1;
 
-
-
-        System.out.println(count);
+        for(int i = 4; i<=n; i++){
+            if(i % 3 == 0) count[i] = count[i/3] +1;
+            if(i % 2 == 0) count[i] = Math.min(count[i], count[i/2] +1);
+            count[i] = Math.min(count[i], count[i-1] +1);
+        }
+        System.out.println(count[n]);
     }
 }
