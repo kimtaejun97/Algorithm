8327604f1638f66264bd6776f798270467adc7ef
kimtaejun <kim_taejun@naver.com>
2021-10-29 19:02:30 +0900

    스택: 괄호

---
+++ src/baekjoon/단계17스택/괄호.java
@@ -0,0 +1,35 @@
+package baekjoon.단계17스택;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+import static java.lang.System.in;
+
+public class 괄호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        Stack<Integer> st;
+
+        for(int i=0; i<t; i++){
+            st = new Stack();
+            char[] brace = br.readLine().toCharArray();
+            for(int j=0; j< brace.length; j++){
+                if(brace[j] == '(') st.push(1);
+                else{
+                    if(st.isEmpty()) {
+                        st.push(1);
+                        break;
+                    }
+                    st.pop();
+                }
+            }
+            if(!st.isEmpty()) sb.append("NO").append("\n");
+            else sb.append("YES").append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
