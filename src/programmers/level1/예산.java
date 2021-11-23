b5043e4ad5e3491e8a628086cd74633d578333e6
kimtaejun <kim_taejun@naver.com>
2021-11-23 16:45:17 +0900

    예산: 최대 배정 - 그리디

---
+++ src/programmers/level1/예산.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+/*
+그리디 
+ */
+public class 예산 {
+    public int solution(int[] department, int budget) {
+        int answer = 0;
+        Arrays.sort(department);
+        for(int d : department){
+            if((budget -= d) >= 0) answer ++;
+            else break;
+        }
+        return answer;
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,14 +1,9 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 
 public class test {
     public static void main(String[] args) throws IOException {
 
     }
-
 }
+
+
