a3835755ef33f9b7c66eaa89ef9b53c56238cd9b
kimtaejun <kim_taejun@naver.com>
2021-11-24 16:32:38 +0900

    문자열내 p와 y의 수: Uppercase, IngnoreCase, String.chars().filter().count()

---
+++ src/programmers/level1/문자열내p와y의개수.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class 문자열내p와y의개수 {
+    boolean solution(String s) {
+        int count = 0;
+        String[] strArr = s.split("");
+        for(String str: strArr){
+            if(str.equalsIgnoreCase("P")) count++;
+            else if(str.equalsIgnoreCase("Y")) count --;
+        }
+        return count == 0? true:false;
+
+        /*
+        s = s.toUpperCase();
+        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+         */
+    }
+}
