73fd6883fab0f677f526b7db38d503f888529bce
kimtaejun <kim_taejun@naver.com>
2021-11-28 17:24:54 +0900

    나머지가 1이되는 수

---
+++ src/programmers/level1/나머지가1이되는수찾기.java
@@ -0,0 +1,14 @@
+package programmers.level1;
+
+public class 나머지가1이되는수찾기 {
+    public int solution(int n) {
+
+        for(int i=2; i<n; i++){
+            if(n % i == 1){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
